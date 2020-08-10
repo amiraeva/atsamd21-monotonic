b5668ae5b272f29a18ce9e12bf7fa55c5ea5f816
Amer Safdari <amersafdari@gmail.com>
2020-08-10 17:06:44 -0500

    continues work

--- examples/blink_1s.rs
+++ examples/blink_1s.rs
@@ -25,20 +25,18 @@ fn main() -> ! {
     let mut pins = peripherals.PORT.split();
     let mut red_led = pins.pa17.into_open_drain_output(&mut pins.port);
 
-    // gclk0 represents a configured clock using the system 48MHz oscillator
-    let gclk0 = clocks.gclk0();
-    // configure a clock for the TC4 and TC5 peripherals
-    let tc45 = &clocks.tc4_tc5(&gclk0).unwrap();
-
-
-    // created fused 32 bit timer
-    let mut tc4tc5_fused =
-        FusedTimerCounter::initialize(peripherals.TC4, peripherals.TC5, tc45, &mut peripherals.PM);
+    // initialzes fused 32 bit timer
+    FusedTimerCounter::initialize(
+        peripherals.TC4,
+        peripherals.TC5,
+        &mut clocks,
+        &mut peripherals.PM,
+    );
 
     loop {
-        if tc4tc5_fused.overflowed() {
-            tc4tc5_fused.reset_ovf_flag();
-            red_led.toggle();
-        }
+        // if tc4tc5_fused.overflowed() {
+        //     tc4tc5_fused.reset_ovf_flag();
+        //     red_led.toggle();
+        // }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,25 @@
 #![no_std]
 
-use atsamd_hal::{
-    clock::Tc4Tc5Clock,
-    target_device::{TC4, TC5, PM},
+use core::{
+    marker::PhantomData,
+    num::NonZeroU32,
+    ops::{self, Add},
 };
 
+use atsamd_hal::{
+    clock::GenericClockController,
+    target_device::{PM, TC4, TC5},
+};
+use ops::Sub;
 pub struct FusedTimerCounter<T1, T2> {
-    tc4: T1,
-    tc5: T2,
+    t1: T1,
+    t2: T2,
 }
 
 impl FusedTimerCounter<TC4, TC5> {
-    pub fn initialize(tc4: TC4, tc5: TC5, tc45_clk: &Tc4Tc5Clock, pm: &mut PM) -> Self {
+    pub fn initialize(tc4: TC4, tc5: TC5, gclk: &mut GenericClockController, pm: &mut PM) {
+        let gclk0 = gclk.gclk0();
+        let tc45 = gclk.tc4_tc5(&gclk0).expect("tc4_tc5 already initialized");
 
         pm.apbcmask.modify(|_, w| w.tc4_().set_bit());
         pm.apbcmask.modify(|_, w| w.tc5_().set_bit()); // don't know if this is necessary
@@ -41,12 +49,12 @@ impl FusedTimerCounter<TC4, TC5> {
         count.readreq.modify(|_, w| w.rcont().set_bit());
 
         // Set TOP value for mfrq mode
-        count.cc[0].write(|w| unsafe { w.cc().bits(46875 / 2) });
+        count.cc[0].write(|w| unsafe { w.cc().bits(u32::max_value()) }); // continue counting and wrapping around u32::MAX
 
         count.ctrla.modify(|_, w| {
             w.mode().count32();
             w.wavegen().mfrq();
-            w.prescaler().div1024();
+            w.prescaler().div1024(); // divides CPU clock speed
             w.enable().set_bit()
         });
 
@@ -59,19 +67,70 @@ impl FusedTimerCounter<TC4, TC5> {
         //     panic!("32 bit mode didn't work for fused counter");
         // }
 
-        Self { tc4, tc5 }
+        let counter = Self {
+            t1: tc4,
+            t2: tc5,
+        };
+
+        unsafe { MONOTONIC_TIMER = Some(counter) };
     }
 
     pub fn overflowed(&self) -> bool {
-        self.tc4.count32().intflag.read().ovf().bit_is_set()
+        self.t1.count32().intflag.read().ovf().bit_is_set()
     }
 
     pub fn reset_ovf_flag(&mut self) {
-        self.tc4.count32().intflag.write(|w| w.ovf().set_bit());
+        self.t1.count32().intflag.write(|w| w.ovf().set_bit());
     }
 
     #[inline]
     pub fn now(&self) -> u32 {
-        self.tc4.count32().count.read().bits()
+        self.t1.count32().count.read().bits()
+    }
+}
+
+static mut MONOTONIC_TIMER: Option<FusedTimerCounter<TC4, TC5>> = None;
+
+pub struct Monotonic;
+
+impl rtic::Monotonic for Monotonic {
+    type Instant = Instant;
+
+    fn ratio() -> rtic::Fraction {
+        todo!()
+    }
+    fn now() -> Self::Instant {
+        todo!()
+    }
+    unsafe fn reset() {
+        todo!()
+    }
+    fn zero() -> Self::Instant {
+        todo!()
+    }
+}
+
+#[derive(Clone, Copy, Ord, PartialOrd, Eq, PartialEq)]
+pub struct Instant(u32);
+
+impl Sub for Instant {
+    type Output = Instant;
+
+    fn sub(self, other: Self) -> Self::Output {
+        let sub = self.0 - other.0;
+        Instant(sub)
+    }
+}
+
+impl From<u32> for Instant {
+    fn from(val: u32) -> Self {
+        Instant(val)
+    }
+}
+
+impl Add<atsamd_hal::time::Miliseconds> for Instant {
+    type Output = Instant;
+    fn add(self, other: atsamd_hal::time::Miliseconds) -> Self::Output {
+        todo!()
     }
 }
